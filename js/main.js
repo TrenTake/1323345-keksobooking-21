2f95aea277d643c53e0300e4d873d9c25d427115
TrenTake <stephanie_bobyr10@mail.ru>
2020-10-16 15:51:16 +0300

    Генерация пинов

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
 'use strict';
 const featuresArr = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+const photosArr = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 
-function shuffle(array) {
+const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
 
     // поменять элементы местами
     // мы используем для этого синтаксис "деструктурирующее присваивание"
-    // подробнее о нём - в следующих главах
     // то же самое можно записать как:
     // let t = array[i]; array[i] = array[j]; array[j] = t
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
+
+const createFeaturesArr = () => {
+  const randomFeatures = featuresArr.slice();
+  shuffle(randomFeatures);
+  randomFeatures.slice(0, getRandomInt(randomFeatures.length));
+}
+
+const createPhotosArr = () => {
+  const randomPhotos = photosArr.slice();
+  shuffle(randomPhotos);
+  randomPhotos.slice(0, getRandomInt(randomPhotos.length));
+}
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
 const createAdv = () => {
-  const avatarNumber = 0;
+  const avatarNumber = getRandomInt(1, 8);
   const title = "";
   const locationArr = [location.x, location.y];
   const price = 10000;
@@ -23,18 +42,7 @@ const createAdv = () => {
   const guestsAmount = 0;
   const checkinTime = ["12:00", "13:00", "14:00"];
   const checkoutTime = ["12:00", "13:00", "14:00"];
-  const createFeaturesArr = () => {
-    const randomFeatures = featuresArr.slice();
-    shuffle(randomFeatures);
-    console.log(randomFeatures);
-  }
   const description = "";
-
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
 
   return {
     author: {
@@ -49,9 +57,9 @@ const createAdv = () => {
         guests: guestsAmount,
         checkin: checkinTime[getRandomInt(0, checkinTime.length)],
         checkout: checkoutTime[getRandomInt(0, checkoutTime.length)],
-//        features: массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
+        features: createFeaturesArr(),
         description: description,
-//        photos: массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
+        photos: createPhotosArr(),
     },
     location: {
         x: getRandomInt(0, 1200),
@@ -61,5 +69,9 @@ const createAdv = () => {
 }
 
 const blockMap = document.querySelector('.map');
+const pinTemplate = document.querySelector('#pin');
+const clonePinTemplate = pinTemplate.cloneNode(true);
 blockMap.classList.remove('map--faded');
 createAdv();
+createFeaturesArr();
+createPhotosArr();
