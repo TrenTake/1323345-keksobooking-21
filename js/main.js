ff8de2cb28113cca997b4228c342a269c5738bdb
TrenTake <stephanie_bobyr10@mail.ru>
2020-10-22 00:40:48 +0300

    Добавление пинов

--- js/main.js
+++ js/main.js
@@ -1,77 +1,98 @@
 'use strict';
-const featuresArr = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-const photosArr = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+const features = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const photos = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const HEIGHT_PIN = 40;
+const WEIGHT_PIN = 40;
+
+/**
+  * Перемешивает массив
+  * https://learn.javascript.ru/task/shuffle
+*/
 
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
-
-    // поменять элементы местами
-    // мы используем для этого синтаксис "деструктурирующее присваивание"
-    // то же самое можно записать как:
-    // let t = array[i]; array[i] = array[j]; array[j] = t
     [array[i], array[j]] = [array[j], array[i]];
   }
-}
+};
 
-const createFeaturesArr = () => {
-  const randomFeatures = featuresArr.slice();
+const pinTemplate = document.querySelector(`#pin`);
+
+
+const createFeatures = () => {
+  const randomFeatures = features.slice();
   shuffle(randomFeatures);
   randomFeatures.slice(0, getRandomInt(randomFeatures.length));
-}
+};
 
-const createPhotosArr = () => {
-  const randomPhotos = photosArr.slice();
+const createPhotos = () => {
+  const randomPhotos = photos.slice();
   shuffle(randomPhotos);
   randomPhotos.slice(0, getRandomInt(randomPhotos.length));
-}
+};
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
+};
 
-const createAdv = () => {
+const createAdvertisement = () => {
   const avatarNumber = getRandomInt(1, 8);
-  const title = "";
-  const locationArr = [location.x, location.y];
-  const price = 10000;
-  const houseType = ["palace", "flat", "house", "bungalow"];
+  const titleValue = ``;
+  const locationValue = [location.x, location.y];
+  const priceValue = 10000;
+  const houseType = [`palace`, `flat`, `house`, `bungalow`];
   const roomsAmount = 0;
   const guestsAmount = 0;
-  const checkinTime = ["12:00", "13:00", "14:00"];
-  const checkoutTime = ["12:00", "13:00", "14:00"];
-  const description = "";
+  const checkinTime = [`12:00`, `13:00`, `14:00`];
+  const checkoutTime = [`12:00`, `13:00`, `14:00`];
+  const descriptionValue = ``;
 
   return {
     author: {
-      avatar: "img/avatars/user0" + avatarNumber + ".png"
+      avatar: `img/avatars/user0` + avatarNumber + `.png`
     },
     offer: {
-        title: title,
-        address: locationArr,
-        price: price,
-        type: houseType[getRandomInt(0, houseType.length)],
-        rooms: roomsAmount,
-        guests: guestsAmount,
-        checkin: checkinTime[getRandomInt(0, checkinTime.length)],
-        checkout: checkoutTime[getRandomInt(0, checkoutTime.length)],
-        features: createFeaturesArr(),
-        description: description,
-        photos: createPhotosArr(),
+      title: titleValue,
+      address: locationValue,
+      price: priceValue,
+      type: houseType[getRandomInt(0, houseType.length)],
+      rooms: roomsAmount,
+      guests: guestsAmount,
+      checkin: checkinTime[getRandomInt(0, checkinTime.length)],
+      checkout: checkoutTime[getRandomInt(0, checkoutTime.length)],
+      features: createFeatures(),
+      description: descriptionValue,
+      photos: createPhotos(),
     },
     location: {
-        x: getRandomInt(0, 1200),
-        y: getRandomInt(130, 630),
+      x: getRandomInt(0, 1200),
+      y: getRandomInt(130, 630),
     }
-  }
+  };
+};
+
+const mapElement = document.querySelector(`.map`);
+mapElement.classList.remove(`map--faded`);
+
+const advertisements = [];
+for (let i = 0; i < 8; i++) {
+  const advertisement = createAdvertisement();
+  advertisements.push(advertisement);
 }
 
-const blockMap = document.querySelector('.map');
-const pinTemplate = document.querySelector('#pin');
-const clonePinTemplate = pinTemplate.cloneNode(true);
-blockMap.classList.remove('map--faded');
-createAdv();
-createFeaturesArr();
-createPhotosArr();
+const fragment = document.createDocumentFragment();
+
+advertisements.forEach((advertisement) => {
+  const pin = pinTemplate.cloneNode(true).content.querySelector(`.map__pin`);
+
+  pin.style.top = (advertisement.location.y - HEIGHT_PIN) + `px`;
+  pin.style.left = (advertisement.location.x - WEIGHT_PIN / 2) + `px`;
+  const imgElement = pin.querySelector(`img`);
+  imgElement.src = advertisement.author.avatar;
+  fragment.appendChild(pin);
+});
+
+const blockMap = document.querySelector(`.map__pins`);
+blockMap.appendChild(fragment);
