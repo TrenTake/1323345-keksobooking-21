f2f8f807929a996f9e03c748058918088db7b961
TrenTake <stephanie_bobyr10@mail.ru>
2020-11-09 11:34:45 +0200

    Исправление ошибок

---
+++ js/main-pin.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(() => {
+  window.pin.mainPinElement.addEventListener(`mousedown`, (evt) => {
+    evt.preventDefault();
+
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    let dragged = false;
+    const onMouseMove = (moveEvt) => {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      let shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      const newCoords = {
+        x: window.pin.mainPinElement.offsetLeft + window.pin.mainPinElement.clientWidth / 2 - shift.x,
+        y: window.pin.mainPinElement.offsetTop + window.pin.mainPinElement.clientHeight - shift.y,
+      };
+
+      if (newCoords.x > 0 && newCoords.x < window.map.mapElement.clientWidth && newCoords.y > 130 && newCoords.y < 630) {
+        window.pin.mainPinElement.style.left = window.pin.mainPinElement.offsetLeft - shift.x + `px`;
+        window.pin.mainPinElement.style.top = window.pin.mainPinElement.offsetTop - shift.y + `px`;
+      }
+    };
+
+    const onMouseUp = (upEvt) => {
+      upEvt.preventDefault();
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
+
+      if (dragged) {
+        const onClickPreventDefault = (clickEvt) => {
+          clickEvt.preventDefault();
+          window.pin.mainPinElement.removeEventListener(`click`, onClickPreventDefault);
+        };
+        window.pin.mainPinElement.addEventListener(`click`, onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
+  });
+})();
--- js/map.js
+++ js/map.js
@@ -24,51 +24,6 @@
     card.remove();
   };
 
-  window.pin.mainPinElement.addEventListener(`mousedown`, (evt) => {
-    evt.preventDefault();
-
-    let startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-
-    let dragged = false;
-    const onMouseMove = (moveEvt) => {
-      moveEvt.preventDefault();
-      dragged = true;
-
-      let shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      window.pin.mainPinElement.style.top = (mapElement.offsetTop - shift.y) + `px`;
-      window.pin.mainPinElement.style.left = (mapElement.offsetLeft - shift.x) + `px`;
-    };
-
-    const onMouseUp = (upEvt) => {
-      upEvt.preventDefault();
-      document.removeEventListener(`mousemove`, onMouseMove);
-      document.removeEventListener(`mouseup`, onMouseUp);
-
-      if (dragged) {
-        const onClickPreventDefault = (clickEvt) => {
-          clickEvt.preventDefault();
-          window.pin.mainPinElement.removeEventListener(`click`, onClickPreventDefault);
-        };
-        window.pin.mainPinElement.addEventListener(`click`, onClickPreventDefault);
-      }
-    };
-
-    document.addEventListener(`mousemove`, onMouseMove);
-    document.addEventListener(`mouseup`, onMouseUp);
-  });
-
   window.map = {
     mapElement,
     openCard,
