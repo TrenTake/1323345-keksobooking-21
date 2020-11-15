0ca0faacac24c4746367d2f3d9848c7bb4c3da1c
TrenTake <stephanie_bobyr10@mail.ru>
2020-11-15 23:28:39 +0200

    Filter

--- js/filter.js
+++ js/filter.js
@@ -2,8 +2,49 @@
 
 (() => {
   const typeInput = document.querySelector(`#housing-type`);
+  const priceInput = document.querySelector(`#housing-price`);
+  const roomInput = document.querySelector(`#housing-rooms`);
+  const guestInput = document.querySelector(`#housing-guests`);
 
-  typeInput.addEventListener(`change`, () => {
+  const Price = {
+    ANY: `any`,
+    MIDDLE: `middle`,
+    LOW: `low`,
+    HIGH: `high`,
+  };
+
+  const checkGuests = (guestsCount) => {
+    return guestInput.value === `any` || parseInt(guestInput.value, 10) === guestsCount;
+  };
+
+  const checkRooms = (roomNumber) => {
+    return roomInput.value === `any` || parseInt(roomInput.value, 10) === roomNumber;
+  };
+
+  const checkType = (type) => {
+    return typeInput.value === `any` || typeInput.value === type;
+  };
+
+  const checkPrice = (price) => {
+    switch (priceInput.value) {
+      case Price.ANY:
+        return true;
+
+      case Price.MIDDLE:
+        return price > 10000 && price < 500000;
+
+      case Price.LOW:
+        return price < 10000;
+
+      case Price.HIGH:
+        return price > 50000;
+
+      default:
+        return true;
+    }
+  };
+
+  const filterAdvertisement = () => {
     window.pin.clearPin();
 
     const cardElement = document.querySelector(`.map__card`);
@@ -12,8 +53,13 @@
     }
 
     let filteredAdvertisements = [];
-    for (const advertisement of window.pin.advertisements.slice()) {
-      if (typeInput.value === advertisement.offer.type) {
+    for (const advertisement of window.pin.advertisements) {
+      if (
+        checkType(advertisement.offer.type)
+        && checkPrice(advertisement.offer.price)
+        && checkRooms(advertisement.offer.rooms)
+        && checkGuests(advertisement.offer.guests)
+      ) {
         filteredAdvertisements.push(advertisement);
       }
 
@@ -22,7 +68,22 @@
       }
     }
     window.pin.pinShow(filteredAdvertisements);
+  };
 
+  guestInput.addEventListener(`change`, () => {
+    filterAdvertisement();
+  });
+
+  priceInput.addEventListener(`change`, () => {
+    filterAdvertisement();
+  });
+
+  roomInput.addEventListener(`change`, () => {
+    filterAdvertisement();
+  });
+
+  typeInput.addEventListener(`change`, () => {
+    filterAdvertisement();
   });
 
 })();
